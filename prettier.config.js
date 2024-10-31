edf716d95a68604a7db5ffa6474562a05b526588
blumsicle <blumsicle@icloud.com>
2024-10-30 21:12:25 -0600

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-organize-imports"],
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
+
+export default config;
