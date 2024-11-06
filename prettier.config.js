750e0983bbc3428002733fe164c6e2bf5ebc72dd
blumsicle <blumsicle@icloud.com>
2024-11-05 19:13:00 -0700

    Use 4 spaces

--- prettier.config.js
+++ prettier.config.js
@@ -2,12 +2,12 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  plugins: ["prettier-plugin-organize-imports"],
-  semi: true,
-  singleQuote: false,
-  tabWidth: 2,
-  trailingComma: "es5",
-  useTabs: false,
+    plugins: ["prettier-plugin-organize-imports"],
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+    useTabs: false,
 };
 
 export default config;
